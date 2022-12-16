51f233dda392910bac02d28ab1d495cf93d1d64b
panjiash <pashriyandhi@gmail.com>
2022-12-16 16:54:26 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -11,6 +11,11 @@
 <body>
     <h1>git remote dari local</h1>
     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt accusantium sequi sapiente eum dolores, alias, modi officiis odit natus praesentium facere nihil sint laudantium maxime quam magnam eos reiciendis recusandae?</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+        <li>tiga</li>
+    </ul>
 </body>
 
 </html>
